0990649a2f97aa5c55628fac13ffdc1c998b61a0
Jeon-ChanYoung <162970543+Jeon-ChanYoung@users.noreply.github.com>
2026-02-12 09:58:50 +0000

    update port

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import uvicorn
 from config.map_config import load_config
 from server import create_app
 
-# 8003
 if __name__ == "__main__":
     config = load_config()
     app = create_app(config)
-    uvicorn.run(app, host="0.0.0.0", port=8003) 
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
